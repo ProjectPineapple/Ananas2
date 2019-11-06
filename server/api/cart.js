98c9e83634ab193643194400ed23f91324b848ed
Frances Tiffin <ftiffin@alumnae.mills.edu>
2019-11-06 12:03:25 -0600

    add userId and sessionId to get relevant cart! :)

--- server/api/cart.js
+++ server/api/cart.js
@@ -3,12 +3,17 @@ const {Order} = require('../db/models')
 module.exports = router
 
 router.get(`/`, async (req, res, next) => {
+  // /api/cart
   try {
+    // validate the current session email or session id
+    console.log('request session: ', req.session)
+    const whereClause = {}
+    whereClause.status = 'in-cart'
+    whereClause.userId = req.user ? req.user.id : null
+    whereClause.sessionId = req.sessionID ? req.sessionID : null
+    // whereClause.sessionId = ????
     const cart = await Order.findOne({
-      where: {
-        status: 'in-cart'
-        // also the association between session and cart
-      }
+      where: whereClause
     })
     res.json(cart)
   } catch (err) {
