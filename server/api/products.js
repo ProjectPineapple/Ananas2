4f77427c8b050f9b27f9d244e2b849480255e731
Jennie-Myhren <jenniewalters2012@u.northwestern.edu>
2019-11-06 10:43:29 -0600

    allProducts api routes aligned wiht singleProduct

--- server/api/products.js
+++ server/api/products.js
@@ -20,38 +20,38 @@ router.get('/', async (req, res, next) => {
 router.post('/', async (req, res, next) => {
   try {
     const product = await Product.create({
-      visible: req.body.stock > 0,
       name: req.body.name,
       description: req.body.description,
       stock: req.body.stock,
       tags: req.body.tags,
       price: req.body.price,
-      photos: req.body.photos
+      photos: [req.body.photo1, req.body.photo2, req.body.photo3]
     })
-    //ASSOCIATION PRODUCT-ORDER
-    //QUERY: WOULD WE NEED THE FOLLOWING? OR MOVE TO API/ORDERS?
-    if (req.body.orders !== undefined) {
-      const orderIds = req.body.orders.map(order => Number(order.id))
+    // //ASSOCIATION PRODUCT-ORDER
+    // //PROB MOVE TO API/ORDERS +/- API/USERS
+    // if (req.body.orders !== undefined) {
+    //   const orderIds = req.body.orders.map(order => Number(order.id))
 
-      //find orders containing the product
-      const ordered = await Order.findAll({
-        where: {id: {[Op.in]: orderIds}}
-      })
+    //   //find orders containing the product
+    //   const ordered = await Order.findAll({
+    //     where: {id: {[Op.in]: orderIds}}
+    //   })
 
-      //add to product instance
-      product.addOrders(ordered)
-    }
+    //   //add to product instance
+    //   product.addOrders(ordered)
+    // }
 
     //ASSOCIATION PRODUCT-REVIEW
-    if (req.body.reviews !== undefined) {
-      const reviewIds = req.body.reviews.map(review => Number(review.id))
+    //DONT THINK WILL BE ADDING REVIEW TO NEWLY CREATED PRODUCT
+    // if (req.body.reviews !== undefined) {
+    //   const reviewIds = req.body.reviews.map(review => Number(review.id))
 
-      const productReviews = await Review.findAll({
-        where: {id: {[Op.in]: reviewIds}}
-      })
+    //   const productReviews = await Review.findAll({
+    //     where: {id: {[Op.in]: reviewIds}}
+    //   })
 
-      product.addReviews(productReviews)
-    }
+    //   product.addReviews(productReviews)
+    // }
 
     res.status(201).json(product)
   } catch (err) {
