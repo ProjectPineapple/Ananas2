5495a244ccd0609b472e19773d3e550b9a964087
Frances Tiffin <ftiffin@alumnae.mills.edu>
2019-11-06 12:01:42 -0600

    viewCart and checkout in reducer

--- client/store/index.js
+++ client/store/index.js
@@ -3,9 +3,9 @@ import {createLogger} from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import user from './user'
-
 import checkout from './checkout'
 import viewCart from './viewCart'
+
 const reducer = combineReducers({user, checkout, viewCart})
 
 const middleware = composeWithDevTools(
