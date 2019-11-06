5e5ae1f9c47b8791813fe1f4eec9020f68f63bce
Frances Tiffin <ftiffin@alumnae.mills.edu>
2019-11-06 10:57:17 -0600

    use /cart router

--- server/api/index.js
+++ server/api/index.js
@@ -2,6 +2,8 @@ const router = require('express').Router()
 module.exports = router
 
 router.use('/users', require('./users'))
+router.use('/orders', require('./orders'))
+router.use('/cart', require('./cart'))
 
 router.use((req, res, next) => {
   const error = new Error('Not Found')
