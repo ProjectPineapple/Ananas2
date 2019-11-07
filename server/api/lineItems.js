fc61b81287161157da61146108914322bc5e662e
Frances Tiffin <ftiffin@alumnae.mills.edu>
2019-11-07 14:07:22 -0600

    routes for lineItems

---
+++ server/api/lineItems.js
@@ -0,0 +1,90 @@
+const router = require('express').Router()
+const {Order, Session, Product, OrderLineItem} = require('../db/models')
+
+module.exports = router
+
+router.get('/', async (req, res, next) => {
+  try {
+    const orders = await Order.findAll({})
+    res.json(orders)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.get('/cart', async (req, res, next) => {
+  try {
+    const session = await Session.findOne({
+      where: {
+        sid: req.sessionID
+      }
+    })
+    const whereClause = {}
+    whereClause.status = 'in-cart'
+    if (req.user) whereClause.userId = req.user.id
+    else whereClause.SessionId = session.id
+    const cart = await Order.findOrCreate({
+      // where: whereClause,
+      where: {id: 1},
+      include: [
+        {
+          model: OrderLineItem
+        },
+        {
+          model: Product
+        }
+      ]
+    })
+    res.json(cart[0])
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.delete('/cart', async (req, res, next) => {
+  try {
+    const session = await Session.findOne({
+      where: {
+        sid: req.sessionID
+      }
+    })
+    const whereClause = {}
+    whereClause.status = 'in-cart'
+    if (req.user) whereClause.userId = req.user.id
+    else whereClause.SessionId = session.id
+    const cart = await Order.findOne({
+      where: whereClause
+    })
+    console.log(req.body)
+    /*const productToDelete = await Product.findOne({
+       where: { id: req.body.productId} 
+       })*/
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.post('/cart', async (req, res, next) => {
+  try {
+    const session = await Session.findOne({
+      where: {
+        sid: req.sessionID
+      }
+    })
+    const whereClause = {}
+    whereClause.status = 'in-cart'
+    if (req.user) whereClause.userId = req.user.id
+    else whereClause.SessionId = session.id
+    const cart = await Order.findOne({
+      where: whereClause
+    })
+    console.log(req.body)
+    /*const productToAdd = await Product.findOne({
+       where: { id: req.body.productId} 
+       })*/
+
+    // cart.addProducts(
+  } catch (err) {
+    next(err)
+  }
+})
