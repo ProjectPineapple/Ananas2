35e16c76a47a9f99ff643f0f18653bad66084002
Antanas Tricou <APTricou@gmail.com>
2019-11-07 16:58:29 -0600

    added a checkout proceed button to ViewCart

--- client/components/ViewCart.js
+++ client/components/ViewCart.js
@@ -1,6 +1,7 @@
 import React, {useEffect} from 'react'
 import {useSelector, useDispatch} from 'react-redux'
-import {Grid, Image, Button} from 'semantic-ui-react'
+import {Grid, Image, Button, Segment} from 'semantic-ui-react'
+import {withRouter} from 'react-router'
 
 // from https://stackoverflow.com/questions/3883342/add-commas-to-a-number-in-jquery
 const commaSeparateNumber = val => {
@@ -10,7 +11,7 @@ const commaSeparateNumber = val => {
   return val
 }
 
-const ViewCart = props => {
+const ViewCart = ({history, match}) => {
   const products = useSelector(state => state.viewCart.products) || []
   const orderLineItems =
     useSelector(state => state.viewCart.OrderLineItems) || []
@@ -29,63 +30,75 @@ const ViewCart = props => {
   return products.length === 0 ? (
     <h1>Your cart is empty!</h1>
   ) : (
-    <Grid padded="horizontally">
-      {products.map(item => (
-        <Grid.Row key={item.id}>
-          <Grid.Column width={4}>
-            <Image src={item.photos[0]} />
-          </Grid.Column>
-          <Grid.Column width={9} />
-          <Grid.Column width={3}>
-            <div>
-              <b>{item.name}</b>
-            </div>
-            {item.stock > 0 ? (
-              <div className="in-stock">
-                <div>Price: ${commaSeparateNumber(item.price / 100)}</div>
-                {orderLineItems.find(lineItem => lineItem.productId === item.id)
-                  .quantity >= item.stock ? (
-                  <div style={{color: 'red'}}>Qty:{' ' + item.stock}</div>
-                ) : (
-                  <div>
-                    Qty:{' ' +
-                      orderLineItems.find(
-                        lineItem => lineItem.productId === item.id
-                      ).quantity}
-                  </div>
-                )}
-                <Button onClick={() => console.log('you clicked `edit`')}>
-                  Edit
-                </Button>
-                <Button onClick={() => console.log('you clicked `remove`')}>
-                  Remove
-                </Button>
+    <div>
+      {match.path === '/cart' ? (
+        <Segment padded>
+          <Button onClick={() => history.push('/cart/checkout')}>
+            Proceed to checkout
+          </Button>
+        </Segment>
+      ) : null}
+      <Grid padded="horizontally">
+        {products.map(item => (
+          <Grid.Row key={item.id}>
+            <Grid.Column width={4}>
+              <Image src={item.photos[0]} />
+            </Grid.Column>
+            <Grid.Column width={9} />
+            <Grid.Column width={3}>
+              <div>
+                <b>{item.name}</b>
               </div>
-            ) : (
-              <div className="out-of-stock">
-                <div style={{color: 'red'}}>
-                  Sorry, this item is out of stock.
+              {item.stock > 0 ? (
+                <div className="in-stock">
+                  <div>Price: ${commaSeparateNumber(item.price / 100)}</div>
+                  {orderLineItems.find(
+                    lineItem => lineItem.productId === item.id
+                  ).quantity >= item.stock ? (
+                    <div style={{color: 'red'}}>Qty:{' ' + item.stock}</div>
+                  ) : (
+                    <div>
+                      Qty:{' ' +
+                        orderLineItems.find(
+                          lineItem => lineItem.productId === item.id
+                        ).quantity}
+                    </div>
+                  )}
+                  <Button onClick={() => console.log('you clicked `edit`')}>
+                    Edit
+                  </Button>
+                  <Button onClick={() => console.log('you clicked `remove`')}>
+                    Remove
+                  </Button>
                 </div>
-                <Button
-                  onClick={() =>
-                    console.log('YEAH, RIGHT! There is no wishlist yet!')
-                  }
-                >
-                  Add to Wishlist
-                </Button>
-                <Button onClick={() => console.log('you clicked `remove`')}>
-                  Remove
-                </Button>
-              </div>
-            )}
-          </Grid.Column>
+              ) : (
+                <div className="out-of-stock">
+                  <div style={{color: 'red'}}>
+                    Sorry, this item is out of stock.
+                  </div>
+                  <Button
+                    onClick={() =>
+                      console.log('YEAH, RIGHT! There is no wishlist yet!')
+                    }
+                  >
+                    Add to Wishlist
+                  </Button>
+                  <Button onClick={() => console.log('you clicked `remove`')}>
+                    Remove
+                  </Button>
+                </div>
+              )}
+            </Grid.Column>
+          </Grid.Row>
+        ))}
+        <Grid.Row>
+          <h2 align="right">
+            Subtotal: ${commaSeparateNumber(subtotal / 100)}
+          </h2>
         </Grid.Row>
-      ))}
-      <Grid.Row>
-        <h2 align="right">Subtotal: ${commaSeparateNumber(subtotal / 100)}</h2>
-      </Grid.Row>
-    </Grid>
+      </Grid>
+    </div>
   )
 }
 
-export default ViewCart
+export default withRouter(ViewCart)
