17b1b69aa9dd57a6a11d35564d4cdbb0e22e1d0c
Frances Tiffin <ftiffin@alumnae.mills.edu>
2019-11-09 15:15:57 -0600

    disables button if quantity reaches remaining stock

--- client/components/AddToCartButton.js
+++ client/components/AddToCartButton.js
@@ -1,22 +1,19 @@
 import React, {useEffect, useState} from 'react'
 import {useDispatch, useSelector} from 'react-redux'
-import {withRouter} from 'react-router'
 
 import {Button, Icon} from 'semantic-ui-react'
 import {addToCart} from '../store/viewCart.js'
 
 const AddToCartButton = props => {
-  const user = useSelector(state => state.user)
+  const {productToAdd, productInCart} = props
+  const {id, stock} = productToAdd
+  let inCart = false
   const orderId = useSelector(state => state.viewCart.id)
-  const lineItems = useSelector(state => state.viewCart.OrderLineItems)
-
   const dispatch = useDispatch()
   const handleClickAdd = event => {
     dispatch(addToCart(+event.target.value, orderId))
   }
-
-  const {productToAdd} = props
-  const {id, stock} = productToAdd
+  if (Object.keys(productInCart).length) inCart = true
 
   return (
     <div>
@@ -25,7 +22,9 @@ const AddToCartButton = props => {
         color="teal"
         onClick={handleClickAdd}
         value={id}
-        disabled={stock <= 0}
+        disabled={
+          stock <= 0 || inCart ? productInCart.quantity >= stock : false
+        }
       >
         <Icon name="cart plus" /> Add to Cart
       </Button>
