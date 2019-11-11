ac2f779fceee12ff85efd6dbb18d2a5f8f5aa674
Frances Tiffin <ftiffin@alumnae.mills.edu>
2019-11-11 13:00:29 -0600

    wip order pagination

--- client/components/orders/OrderList.js
+++ client/components/orders/OrderList.js
@@ -1,11 +1,24 @@
 import React from 'react'
+import {Pagination, Icon} from 'semantic-ui-react'
 import OrderModal from './OrderModal'
+import {parse, stringify} from 'query-string'
 
 const OrderList = props => {
   const {all, orders} = props
 
+  const handlePageChange = (event, data) => {
+    console.log('hi')
+  }
+
   return orders.length ? (
     <div>
+      <Pagination
+        defaultActivePage={1}
+        prevItem={{content: <Icon name="angle left" />, icon: true}}
+        nextItem={{content: <Icon name="angle right" />, icon: true}}
+        totalPages={Math.ceil(orders.length / 10)}
+        onPageChange={handlePageChange}
+      />
       {orders.map(order => (
         <div key={order.id} className="listing-orders">
           <div className="order-name">
